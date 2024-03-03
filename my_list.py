1eba599735cf4c800d892149f064c7da55a63e09
gilbert-ku <gilbert45ku@gmail.com>
2024-03-03 15:52:22 +0300

    completed assignment

---
+++ my_list.py
@@ -0,0 +1,31 @@
+
+
+# empty list
+
+# Create an empty list called my_list
+my_list = []
+
+# Append the elements to my_list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+
+# insert the element in a second position
+my_list.insert(1, 15)
+
+
+# used pop() toremove the last element
+my_list.pop()
+
+
+# sort
+
+my_list.sort()
+
+
+my_list.index(30)
+
+print(my_list.index(30))
+
